1b5247a91d8868b04ae29b8d784357a1909aa895
Gotfrid <gotfridwallenstein@gmail.com>
2022-04-22 17:32:48 +0300

    New class Cat added

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println("Cat's name: " + this.name + ". He ate " + this.food);
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("beefsteak");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
+    }
+}
