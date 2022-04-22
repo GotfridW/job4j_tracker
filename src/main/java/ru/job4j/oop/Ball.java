42a960415f60fbda81bc69e532f489ca39c149e7
Gotfrid <gotfridwallenstein@gmail.com>
2022-04-22 23:25:01 +0300

    New classes Ball, Hare, Wolf, Fox and BallStory added

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public static void tryRun(boolean condition) {
+        if (condition) {
+            System.out.println("Kolobok has been terminated.");
+        } else {
+            System.out.println("Kolobok escaped.");
+        }
+    }
+}
