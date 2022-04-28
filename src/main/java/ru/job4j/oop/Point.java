1b27e91dbfcbb75ad9c613d402a2cbcf3d9c86a3
Gotfrid <gotfridwallenstein@gmail.com>
2022-04-29 02:15:08 +0300

    Method distance3d added and tested.

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -8,13 +8,25 @@ public class Point {
 
     private int y;
 
+    private int z;
+
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
+    }
+
     public double distance(Point that) {
         return sqrt(pow(that.x - this.x, 2) + pow(that.y - this.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(that.x - this.x, 2) + pow(that.y - this.y, 2) + pow(that.z - this.z, 2));
     }
 
     public static void main(String[] args) {
