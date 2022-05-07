9adead0185398602852318437ab532c895518923
Gotfrid <gotfridwallenstein@gmail.com>
2022-05-07 18:02:57 +0300

    2nd & 3rd menu options in method "init" added.

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -17,6 +17,28 @@ public class StartUI {
                 Item item = new Item(name);
                 tracker.add(item);
                 System.out.println("Added item: " + item);
+            } else if (select == 1) {
+            System.out.println("=== Show all items ===");
+            Item[] items = tracker.findAll();
+            if (items.length > 0) {
+                for (Item item : items) {
+                    System.out.println(item);
+                }
+            } else {
+                System.out.println("No items found in storage");
+            }
+        } else if (select == 2) {
+            System.out.println("=== Edit item ===");
+            System.out.print("Enter id: ");
+            int id = Integer.parseInt(scanner.nextLine());
+            System.out.print("Enter name: ");
+            String name = scanner.nextLine();
+            Item item = new Item(name);
+            if (tracker.replace(id, item)) {
+                System.out.println("Item replacement successful.");
+            } else {
+                System.out.println("Item replacement error.");
+            }
             } else if (select == 6) {
                 run = false;
             }
