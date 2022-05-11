0bd59a710badcfdc0d89c5fb33dd3c37f9807100
Gotfrid <gotfridwallenstein@gmail.com>
2022-05-11 22:57:23 +0300

    Optimised imports for tests.

--- src/test/java/ru/job4j/pojo/ShopDropTest.java
+++ src/test/java/ru/job4j/pojo/ShopDropTest.java
@@ -2,7 +2,7 @@ package ru.job4j.pojo;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Test;
 
