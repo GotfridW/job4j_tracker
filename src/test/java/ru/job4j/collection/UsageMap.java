b7ef3fb37d982e5ebc5deaa2b39dc33dc8871b78
Gotfrid <gotfridwallenstein@gmail.com>
2022-06-02 23:25:25 +0300

    UsageMap class added

---
+++ src/test/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("mailbox@mail.com", "Ivanov Ivan Ivanovich");
+        data.put("qwerty@yandex.ru", "Kuznetsov P.V.");
+        data.put("qwerty@yandex.ru", "Kuznetsov Petr Vasilyevich");
+        for (String key : data.keySet()) {
+            System.out.println(data.get(key));
+        }
+    }
+}
