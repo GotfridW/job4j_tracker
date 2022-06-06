939ed3a82a8888505898f964668830a96bf8e91f
Gotfrid <gotfridwallenstein@gmail.com>
2022-06-06 11:19:18 +0300

    method compare refined

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -6,17 +6,14 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int rsl = 0;
-        String shortest = left.length() <= right.length() ? left : right;
-        for (int i = 0; i < shortest.length(); i++) {
+        int shortest = Math.min(left.length(), right.length());
+        for (int i = 0; i < shortest; i++) {
             int cmpByChar = Character.compare(left.charAt(i), right.charAt(i));
             if (cmpByChar != 0) {
                 rsl = cmpByChar;
                 break;
             }
         }
-        if (rsl == 0) {
-            rsl = Integer.compare(left.length(), right.length());
-        }
-        return rsl;
+        return rsl == 0 ? Integer.compare(left.length(), right.length()) : rsl;
     }
 }
