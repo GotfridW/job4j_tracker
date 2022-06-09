5e4806fc4defbce6ffe9269b4dad4111f70327c1
Gotfrid <gotfridwallenstein@gmail.com>
2022-06-09 22:09:32 +0300

    Comments added in BankService; JaCoCo added (from previous task)

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,17 +5,40 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/** Класс описывает работу банковской системы, обрабатывающей базу данных
+ * клинтов и переводы денежных средств между их счетами
+ * @author Gotfrid
+ * @version 1.0
+  */
 public class BankService {
+    /**
+     * users - база данных клиентов
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод проверяет наличие искомого клиента в базе данных и в
+     * случае отстствия добавляет его
+     * @param user клиент
+     */
     public void addUser(User user) {
             users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Метод удаляет из базы данных искомого клиента
+     * @param user клиент
+     */
     public void removeUser(User user) {
         users.remove(user);
     }
 
+    /**
+     * Метод проверяет у клиента наличие искомого счёта
+     * и в случае отсутствия добавляет этот счет
+     * @param passport номер паспорта клиента
+     * @param account банковский счёт клиента
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -25,6 +48,12 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод осуществляет поиск клиента в базе данных по переданным
+     * паспортным данным
+     * @param passport номер паспорта клиента
+     * @return клиент
+     */
     public User findByPassport(String passport) {
         User rsl = null;
         for (User user : users.keySet()) {
@@ -36,6 +65,12 @@ public class BankService {
         return rsl;
     }
 
+    /**
+     * Метод проверяет наличие счёта у клиента
+     * @param passport номер паспора клиента
+     * @param requisite номер банковского счета
+     * @return банковский счёт клиента
+     */
     public Account findByRequisite(String passport, String requisite) {
         Account rsl = null;
         User user = findByPassport(passport);
@@ -49,6 +84,17 @@ public class BankService {
         }
         return rsl;
     }
+
+    /**
+     * Метод проверяет возможность перевода денежных средств со счёта
+     * на счёт и осуществляет его
+     * @param srcPassport номер паспорта отправителя
+     * @param srcRequisite номер счёта отправителя
+     * @param destPassport номер паспорта получателя
+     * @param destRequisite номер счёта получателя
+     * @param amount сумма перевода
+     * @return true или false - статус перевода
+     */
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
