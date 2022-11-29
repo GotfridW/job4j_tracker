be4e32c479542b0d239594f1def9cd53eb0d2d4b
Gotfrid <gotfridwallenstein@gmail.com>
2022-11-29 19:42:35 +0300

    Class refactored

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,7 +1,5 @@
 package ru.job4j.early;
 
-import java.util.function.IntPredicate;
-
 public class PasswordValidator {
     public static String validate(String password) {
         if (password == null) {
@@ -10,44 +8,72 @@ public class PasswordValidator {
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        if (notContains(password, Character::isUpperCase)) {
+        if (!containsUpperCase(password)) {
             throw new IllegalArgumentException(
                     "Password should contain at least one uppercase letter");
         }
-        if (notContains(password, Character::isLowerCase)) {
+        if (!containsLowerCase(password)) {
             throw new IllegalArgumentException(
                     "Password should contain at least one lowercase letter");
         }
-        if (notContains(password, Character::isDigit)) {
+        if (!containsDigit(password)) {
             throw new IllegalArgumentException(
                     "Password should contain at least one figure");
         }
-        if (notContains(password, (c -> !Character.isLetterOrDigit(c)))) {
+        if (!containsSpecialCharacter(password)) {
             throw new IllegalArgumentException(
                     "Password should contain at least one special symbol"
             );
         }
-        if (password.contains("12345")
-                || containsIgnoreCase(password, "qwerty")
-                || containsIgnoreCase(password, "password")
-                || containsIgnoreCase(password, "admin")
-                || containsIgnoreCase(password, "user")) {
-            throw new IllegalArgumentException(
-                    "Password shouldn't contain substrings: qwerty, 12345, password, admin, user"
-            );
+        String[] keyWords = new String[] {
+                "12345", "qwerty", "password", "user", "admin"
+        };
+        for (String word : keyWords) {
+            if (password.toLowerCase().contains(word)) {
+                throw new IllegalArgumentException(
+                        "Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+            }
         }
         return password;
     }
 
-    private static boolean notContains(String str, IntPredicate predicate) {
-        return str.chars().noneMatch(predicate);
+    private static boolean containsUpperCase(String str) {
+        boolean rsl = false;
+        for (char c : str.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
     }
 
-    private static boolean containsIgnoreCase(String str, String other) {
+    private static boolean containsLowerCase(String str) {
         boolean rsl = false;
-        int length = other.length();
-        for (int i = 0; i < str.length() - length; i++) {
-            if (str.regionMatches(true, i, other, 0, length)) {
+        for (char c : str.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    private static boolean containsDigit(String str) {
+        boolean rsl = false;
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    private static boolean containsSpecialCharacter(String str) {
+        boolean rsl = false;
+        for (char c : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(c)) {
                 rsl = true;
                 break;
             }
