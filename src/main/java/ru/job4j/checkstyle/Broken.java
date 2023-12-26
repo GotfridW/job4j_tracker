b1e140de2c3a690089f764cf898574f245ffc352
Gotfrid <gotfridwallenstein@gmail.com>
2023-12-27 01:42:40 +0300

    Corrections on access modifiers

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -6,21 +6,21 @@ public class Broken {
 
     private int sizeOfEmpty = 10;
 
-    public String name;
+    private String name;
 
-    public String surname;
+    private String surname;
 
-    Broken() {
+    public Broken() {
     }
 
-    void echo() { }
+    public void echo() { }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
 }
